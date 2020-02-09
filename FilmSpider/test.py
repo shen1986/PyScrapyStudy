935bc126e2f8b19dcf7f8e0eb35799463d29940c
shenxf1986 <185693240@qq.com>
2020-02-09 21:59:31 +0800

    update

--- FilmSpider/test.py
+++ FilmSpider/test.py
@@ -9,8 +9,38 @@
 #
 # pass
 
-import requests
+# import requests
+#
+# response = requests.get("https://www.dytt8.net/")
+#
+# print(response.text)
 
-response = requests.get("https://www.dytt8.net/")
+# from fake_useragent import UserAgent
+# ua = UserAgent(verify_ssl=False)
 
-print(response.text)
+# print(ua.random)
+
+from selenium.webdriver import Chrome
+import  time
+
+browser = Chrome(executable_path='/Users/shen112/downloads/common/selenium-chromedriver/chromedriver')
+
+browser.get('https://www.zhihu.com/')
+
+time.sleep(3)
+
+browser.find_element_by_css_selector(".SignFlow-tabs .SignFlow-tab:nth-child(2)").click()
+time.sleep(3)
+
+browser.find_element_by_css_selector("input[name=username]").send_keys("15601980930")
+time.sleep(3)
+
+browser.find_element_by_css_selector("input[name=password]").send_keys("shen5801A")
+
+time.sleep(3)
+
+browser.find_element_by_css_selector(".SignFlow-submitButton").click()
+
+print(browser.page_source)
+
+browser.quit()
